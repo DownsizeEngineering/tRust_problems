c24d7d77d5fab8559d945d5c9e57adefdf958c1c
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-14 00:23:32 -0700

    implement dynamic programming solution for modulo_buddies

--- src/modulo_buddies.rs
+++ src/modulo_buddies.rs
@@ -11,7 +11,8 @@ pub fn run (nums: Vec<u32>) -> Vec<u32> {
 
     let mut vec = nums.to_vec();
     vec.sort();
-    backtrack(vec)   
+    // backtrack(vec)   
+    dp(vec)
 }
 
 fn backtrack(nums: Vec<u32>) -> Vec<u32> {
@@ -40,4 +41,37 @@ fn backtrack(nums: Vec<u32>) -> Vec<u32> {
     }
 
     output
+}
+
+fn dp(nums: Vec<u32>) -> Vec<u32> {
+    use std::collections::HashMap;
+    type ValCount = (u32, usize);
+
+    let mut output = Vec::new();
+    let mut paths: HashMap<u32, ValCount> = HashMap::new();
+    let mut max: ValCount = (0, 0);
+    
+    for i in 0..nums.len() {
+        let num = nums[i];
+        let mut path: ValCount = (num, 0);
+        for j in 1..=i {
+            let cur = nums[i - j];
+            let curl = paths.get(&cur).unwrap().1;
+            if num % cur == 0 && curl >= path.1 {
+                path = (cur, curl + 1);
+            }
+        }
+        if path.1 >= max.1 {max = (num, path.1);}
+        paths.insert(num, path);
+    }
+    
+    let mut num = max.0;
+    loop {
+        output.push(num);
+        let path = paths.get(&num).unwrap();
+        if path.1 == 0 {break;}
+        num = path.0;
+    }
+
+    output.into_iter().rev().collect()
 }
