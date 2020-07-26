02fe87b06c988f802541db21f7720ce672246a6e
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-07-26 08:41:38 -0700

    add O(1) solution for digital root

--- src/add_digits.rs
+++ src/add_digits.rs
@@ -1,15 +1,6 @@
 pub fn run() -> i32 {
     pub fn add_digits(num: i32) -> i32 {
-        let mut num = num;
-        let mut output = 0;
-        while num > 0 {
-            let digit = num % 10;
-            num -= digit;
-            output += digit;
-            if output > 9 { output -= 9; }
-            num /= 10;
-        }
-        output
+        num - 9 * ((num -1 ) / 9)
     }
     add_digits(5432)
 }
